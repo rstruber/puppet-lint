61bbbca3b046e3c49c71d153d0e535234ad0e88e
Ryan Struber <rstruber@barracuda.com>
2017-05-31 09:40:28 -0400

    Use a regex to detect autoloader layout rather than a simple path ending check, this brings the check more into line with how r10k will breakup the module name and supports using the autoloader check on paths that have a prefix

--- lib/puppet-lint/plugins/check_classes/autoloader_layout.rb
+++ lib/puppet-lint/plugins/check_classes/autoloader_layout.rb
@@ -11,16 +11,16 @@ PuppetLint.new_check(:autoloader_layout) do
         split_title = title_token.value.split('::')
         mod = split_title.first
         if split_title.length > 1
-          expected_path = "/#{mod}/manifests/#{split_title[1..-1].join('/')}.pp"
+          expected_path = /\/([a-zA-Z0-9]+-)?#{mod}\/manifests\/#{split_title[1..-1].join('/')}\.pp$/
         else
-          expected_path = "/#{title_token.value}/manifests/init.pp"
+          expected_path = /\/([a-zA-Z0-9]+-)?#{title_token.value}\/manifests\/init\.pp$/
         end
 
         if PuppetLint.configuration.relative
-          expected_path = expected_path.gsub(/^\//,'').split('/')[1..-1].join('/')
+          expected_path = /\/#{expected_path.source.gsub(/^\//,'').split('/')[1..-1].join('/')}/
         end
 
-        unless fullpath.end_with? expected_path
+        unless fullpath.match(expected_path)
           notify :error, {
             :message => "#{title_token.value} not in autoload module layout",
             :line    => title_token.line,
